563b2486867b6dd1be0ecae6e6b0d6d8b98bc642
Kelmfaker <kelmfaker@gmail.com>
2024-11-22 00:06:57 +0000

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+"""
+This module defines the User model and sets up the database.
+"""
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-"""
-create a user model
-"""
 
 
 class User(Base):
+    """
+    User model for the users table.
+    Attributes:
+        id (int):The primary key.
+        email (str): The user's email address, non-nullable.
+        hashed_password (str): The user's hashed password,non-nullable.
+        session_id (str): The user's session ID, nullable.
+        reset_token (str): The user's reset token, nullable.
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
